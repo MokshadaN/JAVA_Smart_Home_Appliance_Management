7d500631e6aa59dc1a497dae1743cf77adfafce5
Muskan-gupta04 <mg3285632@gmail.com>
2025-02-12 12:38:27 +0530

    Update file2.java

--- file2.java
+++ file2.java
@@ -1,6 +1,26 @@
 package project
 
 class Chimney extends Appliance{
-  
+  String speed;
+  boolean light_status;
+  public Chimney(boolean power, String speed, boolean light_status){
+    if(power){
+      on();
+    }else{
+      off();
+    }
+
+    this.speed = "low";
+    this. light_status= " false";
+  }
+  public void setFanSpeed(String speed){
+    this.speed =speed;
+  }
+   public void turnOnLight(boolean light_status){
+    this.light_status="true";
+  }
+    public void turnOffLight(boolean light_status){
+    this.light_status="false";
+  }
 }
   
