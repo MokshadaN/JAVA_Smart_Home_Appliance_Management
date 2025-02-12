669647b8202d050ada2bea6f739b59e9a576f98f
manaswijain28 <tiyajain732@gmail.com>
2025-02-12 12:20:32 +0530

    Update appliance.java

--- appliance.java
+++ appliance.java
@@ -1,1 +1,24 @@
+class Appliance{
+  public boolean power = false;
+  public void on(){
+    power = true;
+  }
+  public void off(){
+    power = false;
+  }
+}
 
+class Fan extends Appliance{
+  public double speed;
+  public Fan(boolean power,double speed){
+    if(power){
+      on();
+    }else{
+      off();
+    }
+
+    this.speed = speed;
+  }
+}
+  
+  
