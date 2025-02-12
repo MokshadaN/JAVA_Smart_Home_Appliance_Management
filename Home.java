0d8eeca2deacd654cb9a80b5e85b696ae8822118
Mokshada Nehete <147795188+MokshadaN@users.noreply.github.com>
2025-02-12 12:13:32 +0530

    Created HOME class

--- Home.java
+++ Home.java
@@ -1,3 +1,14 @@
+class HOME{
+  String name;
+  HOME(String name_val){
+    this.name = name_val;
+  }
+}
+
+class Bedroom{
+  
+}
+
 public class HOME{
   public static void main(String[] args){
     System.out.println("Hello")
